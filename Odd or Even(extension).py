34e2c24f23ae80ff888b3f54141725e6338413d9
usmannA <usmannalam@hotmail.co.uk>
2019-08-31 10:58:54 +0100

    Create Odd or Even(extension).py

---
+++ Odd or Even(extension).py
@@ -0,0 +1,14 @@
+''' program that takes user input of two numbers to see if one can be wholly divisible by the other, if this isn't the case the remainder is printed'''
+
+
+num = int(input("choose a number: "))
+check = int(input("choose another to divide your first number with: "))
+
+remainder = str (num % check)
+
+if num % check == 0:
+  print ("the second number you have chosen goes evenly into the first ")
+
+else:
+  print("the second number does not go evenly into the first")
+  print ("the remainder is: " + remainder)
